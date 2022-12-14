01686765fbe2213583d05d64de7e58d671ee8ea5
Bhavya Bhut <pbhavy4@gmail.com>
2022-12-15 02:04:48 +0530

    add : meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Router from 'next/router';
+import Head from 'next/head';
 import NProgress from 'nprogress';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
@@ -23,7 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
-      id='googletagmanager'
+        id='googletagmanager'
         strategy='lazyOnload'
         src={`https://www.googletagmanager.com/gtag/js?id=G-LXTVRQQRSX`}
       />
@@ -38,6 +39,12 @@ export default function App({ Component, pageProps }: AppProps) {
             });
         `}
       </Script>
+      <Head>
+        <meta
+          name='google-site-verification'
+          content='chd242Usl4NJ6ZOOkJvjmWlPyfs_Mj0zPxKD-Z4oQPI'
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
